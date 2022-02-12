8a073ab865929c2de7ad743e4405643b418045d4
unknown <ngodinh.minhtue@gmail.com>
2022-02-13 00:09:29 +0100

    added sample unpkg plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,45 @@
-import { useEffect, useRef, useState } from "react"
-import ReactDOM from 'react-dom'
-import * as esbuild from 'esbuild-wasm'
+import { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-    const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
-    const transpiler = useRef<any>()
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const transpiler = useRef<any>();
 
-    const startService = async () => {
-        transpiler.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        })
-    }
+  const startService = async () => {
+    transpiler.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
 
-    const transpile = async () => {
-        if(!transpiler.current) return 
-        const res = await transpiler.current.transform(input, {loader : 'jsx', target: 'es2015'})
-        setCode(res.code)
-    }
+  const transpile = async () => {
+    if (!transpiler.current) return;
+    const res = await transpiler.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    setCode(res.outputFiles[0].text);
+  };
 
-    useEffect(()=> {
-        startService()
-    }, [])
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>)=> {
-        setInput(event.target.value)
-    }
-    return <div>
-        <textarea value={input} onChange={onChange}></textarea>
-        <button onClick={transpile}>Click</button>
-        <p>{code}</p>
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  };
+  return (
+    <div>
+      <textarea value={input} onChange={onChange}></textarea>
+      <button onClick={transpile}>Click</button>
+      <p>{code}</p>
     </div>
-}
+  );
+};
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
