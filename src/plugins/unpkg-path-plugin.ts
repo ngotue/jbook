3fccd2883fbf2af088d022bb3ab536b886126877
unknown <ngodinh.minhtue@gmail.com>
2022-02-16 00:59:36 +0100

    76 trying nested import

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,31 +1,44 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log('onResolve ::', args)
+        if (args.path === "index.js")
+          return { path: args.path, namespace: "a" };
+        else if (/^https:\/\/unpkg.com/.test(args.importer)) {
+          const link = new URL(args.path, args.importer+'/')
+          return {
+            namespace: "a",
+            path: link.href,
+          };
+        } else {
+          return {
+            path: `https://unpkg.com/${args.path}`,
+            namespace: "a",
+          };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
 
         if (args.path === "index.js") {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
